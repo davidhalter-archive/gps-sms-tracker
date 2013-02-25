4a423b0b377f33bcf7609ee750da8d377182e772
David Halter <davidhalter88@gmail.com>
2013-02-26 01:10:45 +0430

    added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+# show all commands
+set -v
+
+sudo aptitude install gammu python-gammu
+sudo aptitude install postgresql
+
+# create postgres user with the current name
+sudo -u postgres createuser `whoami` -s
+# set a postgres password
+psql -c "alter user `whoami` password '`whoami`'"
+
+# create gps_sms database
+createdb gps_sms
+
+# now do the python requirements
+sudo pip install -r requirements.txt
